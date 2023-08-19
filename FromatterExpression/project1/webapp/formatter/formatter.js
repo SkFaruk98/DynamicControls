f6dfc694e4158cd859ecc8d6349cc14f3a09a164
SkFaruk98 <faruk16014@gmail.com>
2023-08-19 19:41:05 +0530

    initial push

--- FromatterExpression/project1/webapp/formatter/formatter.js
+++ FromatterExpression/project1/webapp/formatter/formatter.js
@@ -1,10 +1,31 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-function(Controller){
-    "use strict";
-   return onclick=function(){
-            
-   }
-       
+sap.ui.define([], 
+    
+    function () {
+	"use strict";
+	return {
+      calMarks:function(aEdu){
+          let marks=0;
+          aEdu.forEach(oEdu=>{
+            marks+=Number(oEdu.Aggregation);
+          })
+          if(marks==0)
+           return `Total Percentage: ${0}`
+          else{
+          let average=Math.floor(marks/aEdu.length);
+          return `Total Percentage: ${average}`;
+          }
+      },
+
+      calExperience:function(aEmp){
+          let experience=0;
+          aEmp.forEach(oEmp=>{
+            if(oEmp.YearOfEnd!="-")
+            experience+=(oEmp.YearOfEnd)- (oEmp.YearOfJoin);
+            else
+            experience+=new Date().getFullYear()-(oEmp.YearOfJoin)
+          })
+          return `Year of Exp: ${experience}`;
+      }
+    }
+
 })
